36c02a994d850eabb68b6343d5076b9a22f1b634
Margaret-Mwangi <developer.margaret@gmail.com>
2023-02-24 22:58:07 +0300

    sum of integers in an array

---
+++ operations.py
@@ -0,0 +1,11 @@
+#Given an array of integers, find the sum of its elements.
+#For example, if the array ar = [1,2,3],
+#Calculate 1+2+3 = 6, so return 6
+#create an array of integers
+#access the elements in the array
+#add the elements in the array
+arr = [1,2,3]
+array_sum = 0
+for x in arr:
+    array_sum = array_sum + x
+print(array_sum)
